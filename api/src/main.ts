2aecdee0df3ff57878c68e5ae9ca3e91bbc8abfc
Douglas Felchilcher <douglasfelc@gmail.com>
2024-08-21 20:47:49 -0300

    cors

--- api/src/main.ts
+++ api/src/main.ts
@@ -4,6 +4,13 @@ import { PokedexApiModule } from './pokedex-api.module';
 async function bootstrap() {
   const app = await NestFactory.create(PokedexApiModule);
   app.setGlobalPrefix('api');
+
+  app.enableCors({
+    origin: 'http://localhost:4200',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization',
+  });
+
   await app.listen(3000);
 }
 
