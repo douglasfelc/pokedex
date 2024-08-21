142eaa4bf4a1d37c7367262ec61f4cb8b29f0d5c
Douglas Felchilcher <douglasfelc@gmail.com>
2024-08-21 19:05:30 -0300

    get pokemon(s)

--- api/src/pokemon/pokemon.service.ts
+++ api/src/pokemon/pokemon.service.ts
@@ -1,4 +1,24 @@
+import { HttpService } from "@nestjs/axios";
 import { Injectable } from "@nestjs/common";
+import { firstValueFrom } from 'rxjs';
 
 @Injectable()
-export class PokemonService {}
+export class PokemonService {
+    private readonly apiUrl = 'https://pokeapi.co/api/v2/pokemon';
+
+    constructor(private readonly httpService: HttpService) {}
+  
+    async getPokemonList(limit: number, offset: number): Promise<any> {
+      const response = await firstValueFrom(
+        this.httpService.get(`${this.apiUrl}?limit=${limit}&offset=${offset}`),
+      );
+      return response.data;
+    }
+  
+    async getPokemonDetails(name: string): Promise<any> {
+      const response = await firstValueFrom(
+        this.httpService.get(`${this.apiUrl}/${name}`),
+      );
+      return response.data;
+    }
+}
